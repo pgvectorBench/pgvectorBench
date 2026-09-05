4db1a41fd26d1c88c2f6cc1ee1f1c6881f60a2db
Zhao Junwang <zhjwpku@gmail.com>
2026-09-05 23:11:11 +0800

    fix(ci): omit optional pgvector LLVM bitcode

--- tests/install_pgvector.sh
+++ tests/install_pgvector.sh
@@ -9,6 +9,8 @@ git -C "$pgvector_test_source" fetch --quiet --depth=1 \
   https://github.com/pgvector/pgvector.git 778dacf20c07caf904557a88705142631818d8cb
 git -C "$pgvector_test_source" checkout --quiet FETCH_HEAD
 pgvector_test_config="$(command -v pg_config)"
-make -C "$pgvector_test_source" -j2 PG_CONFIG="$pgvector_test_config"
-sudo make -C "$pgvector_test_source" PG_CONFIG="$pgvector_test_config" install
+# These correctness tests do not need optional LLVM bitcode. The PGXS package
+# can reference a clang version that is not installed on the runner.
+make -C "$pgvector_test_source" -j2 PG_CONFIG="$pgvector_test_config" with_llvm=no
+sudo make -C "$pgvector_test_source" PG_CONFIG="$pgvector_test_config" with_llvm=no install
 psql -X -v ON_ERROR_STOP=1 -d postgres -c 'CREATE EXTENSION IF NOT EXISTS vector'
